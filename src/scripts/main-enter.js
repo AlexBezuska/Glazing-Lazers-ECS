6ab584695a4399d8db3e318080ef17f3c79afd8d
Alex Bezuska <abezuska@gmail.com>
2015-12-05 21:05:38 -0500

    add origin point for player

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,4 +1,7 @@
 "use strict";
 
 module.exports = function(data) { // eslint-disable-line no-unused-vars
+	var player = data.entities.entities[0];
+	player.position.x = (data.canvas.width/2) - (player.size.width/2);
+	player.position.y = data.canvas.height * 0.8;
 };
